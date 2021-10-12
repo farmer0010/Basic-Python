e1743e193fd7aa9a2a7c0d0579fa88d8fa83fb58
farmer0010 <zmqkf16@naver.com>
2021-10-12 12:24:59 +0900

    basic python0507

---
+++ code 05-07.py
@@ -0,0 +1,16 @@
+score=int(input("점수를 입력하세요:"))
+
+if score >= 90:
+    print ("당신의 점수는 'A'입니다.")
+elif score >= 80:
+        print("당신의 점수는 'B'입니다.")
+elif score >= 70:
+            print("당신의 점수는 'c'입니다.")       
+elif score >= 60:
+                print("당신의 점수는 'd'입니다.")
+else print("당신은 낙제입니다.")
+            
+print("당신의 학점입니다.")
+        
+            
+    
