b6b41d34bf37df6c9d160a1ed42b0354402f6190
farmer0010 <zmqkf16@naver.com>
2021-11-06 23:47:15 +0900

    python class2

---
+++ code class2.py
@@ -0,0 +1,17 @@
+class Car :
+
+        color = ""
+
+        speed = 0
+
+ 
+
+    myCar1 = Car()
+
+    myCar1.color = "빨강"
+
+    myCar1.speed = 30
+
+ 
+
+    print("자동차1의 색상의 %s이며, 현재 속도는 %dkm입니다." % (myCar1.color, myCar1.speed))
